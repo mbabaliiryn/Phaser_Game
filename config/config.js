2d01b428d291cc61341f2ed654fdc5391946c0c2
mbabali iryn <mbabaliiryn@gmail.com>
2020-08-29 09:20:43 +0300

    add phaser arcade config

---
+++ config/config.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
+import Phaser from 'phaser';
+
+export default {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+};
